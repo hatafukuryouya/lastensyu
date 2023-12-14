6680348a06d1f5e4738a81856cf3e8f64c94ed90
ryouya <r.hatafuku.sys22@morijiyobi.ac.jp>
2023-12-14 14:59:41 +0900

    最初のコミット

---
+++ src/main/java/jp/ac/morijyobi/lastensyuu/LastensyuuApplication.java
@@ -0,0 +1,13 @@
+package jp.ac.morijyobi.lastensyuu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastensyuuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LastensyuuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jp/ac/morijyobi/lastensyuu/LastensyuuApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.morijyobi.lastensyuu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastensyuuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
